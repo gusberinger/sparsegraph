d65964fbd4292e87fff0cdad41ca9d50e95a9d3d
Gus Beringer <gusberinger@users.noreply.github.com>
2023-12-12 14:22:36 -0600

    Add sphinx documentation (#2)
    * better docstring for katz centrality
    
    * better docstring for betweeenness centrality
    
    * better docstring for closeness centrality
    
    * better docstring for bfs
    
    * better docstring for radius estimate
    
    * fixed errors in documentation
    
    * ran build commands

--- sparsegraph/algorithms/katz_centrality.py
+++ sparsegraph/algorithms/katz_centrality.py
@@ -13,6 +13,26 @@ def katz_centrality(
     normalized: bool = True,
     verbose: bool = False,
 ):
+    r"""
+    Find Katz centrality defined by :math:`C_{\textrm{Katz}}(i)  = \sum_j  (I_{ij} + \alpha A_{ij} + \alpha^2 A_{ij}^2 + \alpha^3 A_{ij}^3 + \dots)`
+
+    Parameters
+    ----------
+    graph:
+        A SparseGraph graph.
+    alpha:
+        The attenuation factor. The attenuation factor must satisfy :math:`\alpha < \frac{1}{\max(\lambda_1, \dots, \lambda_n)}` or the algorithm will not converge.
+    beta:
+        The constant factor. Defaults to 1.
+    max_iter:
+        The maximum number of iterations to perform before raising a RuntimeError. Defaults to 10,000.
+    tol:
+        The tolerance for convergence. Defaults to 1.0e-6.
+    normalized:
+        If ``True`` the centrality scores are normalized.
+    verbose:
+        If ``True`` a progress bar is displayed during the power iteration.
+    """
     A = graph.adjacency.transpose()
     n = graph.size
     e = np.ones((n, 1))
