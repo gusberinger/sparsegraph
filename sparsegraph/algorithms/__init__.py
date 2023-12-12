f0eb71415c58d0adece193d5b82507c214fab245
Gus Beringer <gusberinger@users.noreply.github.com>
2023-12-12 00:37:10 -0600

    Implement betweeness centrality (#1)
    * implement betweeness centrality
    
    * add tests for non normal
    
    * implement normalization

--- sparsegraph/algorithms/__init__.py
+++ sparsegraph/algorithms/__init__.py
@@ -1,6 +1,7 @@
 from sparsegraph.algorithms.katz_centrality import katz_centrality
 from sparsegraph.algorithms.breadth_first_search import distance_from
 from sparsegraph.algorithms.closeness_centrality import estimate_closeness_centrality
+from sparsegraph.algorithms.betweenness_centrality import betweenness_centrality
 from sparsegraph.algorithms.estimate_radius import (
     estimate_diameter,
     estimate_radius,
@@ -9,6 +10,7 @@ from sparsegraph.algorithms.estimate_radius import (
 
 __all__ = [
     "katz_centrality",
+    "betweenness_centrality",
     "estimate_closeness_centrality",
     "distance_from",
     "estimate_diameter",
---
+++ sparsegraph/algorithms/betweenness_centrality.py
@@ -0,0 +1,55 @@
+import numpy as np
+from tqdm import tqdm
+import sparsegraph as sg
+from collections import deque
+
+
+def betweenness_centrality(
+    graph: sg.SparseGraph,
+    *,
+    normalized=False,
+    verbose=False,
+):
+    """
+    Implements Brande's algorithm
+    https://doi.org/10.1080/0022250X.2001.9990249
+    """
+    indices = graph.adjacency.indices
+    indptr = graph.adjacency.indptr
+    size = graph.adjacency.shape[0]
+    scores = np.zeros(size)
+
+    for s in tqdm(range(size), disable=not verbose):
+        S = []
+        P = [[] for _ in range(size)]
+        sigma = np.zeros(size)
+        sigma[s] = 1
+        d = np.ones(size) * -1
+        d[s] = 0
+        Q = deque()
+        Q.append(s)
+        while Q:
+            v = Q.popleft()
+            S.append(v)
+            for w in indices[indptr[v] : indptr[v + 1]]:
+                if d[w] < 0:
+                    Q.append(w)
+                    d[w] = d[v] + 1
+                if d[w] == d[v] + 1:
+                    sigma[w] += sigma[v]
+                    P[w].append(v)
+
+        delta = np.zeros(size)
+        while S:
+            w = S.pop()
+            for v in P[w]:
+                delta[v] = delta[v] + sigma[v] / sigma[w] * (1 + delta[w])
+            if w != s:
+                scores[w] += delta[w]
+
+    scores /= 2
+
+    if normalized:
+        return (scores / ((size - 1) * (size - 2))) * 2
+    else:
+        return scores
